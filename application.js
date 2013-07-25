090e1c4bd0daaf98f2a1539e33d76201d0fa8ba9
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-25 13:58:57 -0700

    Abstracted renders

--- application.js
+++ application.js
@@ -1,33 +1,54 @@
 $(document).ready(function() {
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    dice.push(new Die());
+    render_all(dice,$('.dice'));
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
+    $.each(dice,function(key,value) {
+      value.roll();
     });
+    render_all(dice,$('.dice'));
   });
 });
 
+var dice = []
+
+// Dice.prototype = new Array
+
+// Dice.prototype.render
+
 function Die() {
   this.value = 0;
+}
+
+function render_all(collection,dom_element) {
+  dom_element.empty();
+  $.each(collection,function(key,value) {
+      value.add_to(dom_element);
+  });
 }
 
 Die.prototype.render = render;
 Die.prototype.roll = roll;
 Die.prototype.add_to = add_to;
 
+function add_to(element) {
+  element.append(this.render());
+}
+
 function render() {
-  return '<div class="die">' + this.value + '</div>'
+  die_value = this.value;
+  return '<div class="die">' + die_value + '</div>'
 }
 
 function roll() {
   this.value = Math.floor((Math.random()*6)+1)
 }
 
-function add_to(element) {
-  element.append(this.render)
+function render_all(collection,dom_element) {
+  dom_element.empty();
+  $.each(collection,function(key,value) {
+      value.add_to(dom_element);
+  });
 }
